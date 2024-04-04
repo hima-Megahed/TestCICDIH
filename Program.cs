987ea864a668780cd93befbbefbbf2b1fdc00bdc
Ibrahim Hasan <Ibrahim__Hasan@outlook.com>
2024-04-04 20:52:09 +0300

    add swagger

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace TestCICDIH
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            //if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
