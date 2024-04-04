1477736ad3ae0ff61b8b0082240135be7d2921a3
Ibrahim Hasan <Ibrahim__Hasan@outlook.com>
2024-04-04 20:54:48 +0300

    swagger

--- Program.cs
+++ Program.cs
@@ -20,7 +20,11 @@ namespace TestCICDIH
             //if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My service");
+                    //c.RoutePrefix = string.Empty;  // Set Swagger UI at apps root
+                });
             }
 
             app.UseHttpsRedirection();
