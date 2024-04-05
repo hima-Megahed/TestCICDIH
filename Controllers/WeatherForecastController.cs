8dfb1365b8d008ca631d91a45425fc5c700c0c75
Ibrahim Hasan <Ibrahim__Hasan@outlook.com>
2024-04-05 17:56:58 +0300

    f2

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -21,7 +21,7 @@ namespace TestCICDIH.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IActionResult Get()
         {
-            return Ok("V3");
+            return Ok("V4");
         }
     }
 }
